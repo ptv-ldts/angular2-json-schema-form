04895697724a82338285240672416e1dc6eeb6b8
AngryBeaver <AngryBeaver@verplanmich.net>
2017-03-30 13:34:20 +0200

    Update to angular 4 Remove Material as it no longer works

--- src/library/json-schema-form.module.ts
+++ src/library/json-schema-form.module.ts
@@ -1,14 +1,12 @@
 import { NgModule, ModuleWithProviders, ANALYZE_FOR_ENTRY_COMPONENTS } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
 
 import { JsonSchemaFormComponent } from './json-schema-form.component';
 import { OrderableDirective } from './utilities/orderable.directive';
 
 import { ALL_FRAMEWORKS } from '../frameworks/index';
 import { ALL_WIDGETS } from '../widgets/index';
-import { ALL_MATERIAL_DESIGN_WIDGETS } from '../frameworks/material-design/index';
 
 import { FrameworkLibraryService } from '../frameworks/framework-library.service';
 import { WidgetLibraryService } from '../widgets/widget-library.service';
@@ -25,18 +23,18 @@ export {
 };
 
 const ALL_COMPONENTS = [
-  ...ALL_FRAMEWORKS, ...ALL_WIDGETS, ...ALL_MATERIAL_DESIGN_WIDGETS,
+  ...ALL_FRAMEWORKS, ...ALL_WIDGETS,
 ];
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule, MaterialModule,
+    CommonModule, FormsModule, ReactiveFormsModule,
   ],
   declarations: [
     JsonSchemaFormComponent, OrderableDirective, ...ALL_COMPONENTS,
   ],
   exports: [
-    FormsModule, ReactiveFormsModule, MaterialModule,
+    FormsModule, ReactiveFormsModule,
     JsonSchemaFormComponent, OrderableDirective,
   ],
 })
